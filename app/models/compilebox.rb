ffa31badbef8601aee4fd8c786ee0c428914cd68
tomwerner <tom-werner@uiowa.edu>
2015-10-28 23:18:35 +0000

    Finished flushing out compilebox ruby interface and tests

--- app/models/compilebox.rb
+++ app/models/compilebox.rb
@@ -1,9 +1,27 @@
-class Compilebox
+class Compilebox 
     include HTTParty
+    
+    class Compilebox::NetworkError < RuntimeError; end;
+
+    
     @@base_uri = '54.149.179.253'
     
     def self.get_output(language, code, stdin)
-       @@response = HTTParty.post("http://#{@@base_uri}/compile", :body => {"language" => language, "code" => code, "stdin" => stdin})
+        begin
+            @@response = HTTParty.post("http://#{@@base_uri}/compile", :body => {"language" => language, "code" => code, "stdin" => stdin})
+            return self.response
+        rescue Errno::ECONNREFUSED => e
+            raise Compilebox::NetworkError.new(e)
+        end
+    end
+    
+    def self.get_languages
+        begin
+            @@response = HTTParty.get("http://#{@@base_uri}/languages")
+            return self.response["languages"]
+        rescue Errno::ECONNREFUSED => e
+            raise Compilebox::NetworkError.new(e)
+        end
     end
     
     def self.response
@@ -12,5 +30,55 @@ class Compilebox
     
     def self.base_uri= base_uri
         @@base_uri = base_uri
+    end
+    
+    def self.get_test_pairs(question_id)
+        return [] # get from Questions, [[stdin, stdout], [stdin, stdout]]
+    end
+    
+    def self.submit_code(question_id, language_id, code)
+        test_pairs = self.get_test_pairs(question_id)
+        results = []
+        
+        test_pairs.each do |test_pair|
+            self.get_output(language_id, code, test_pair[0])
+            if self.error?
+                results.push(self.error_message)
+            elsif self.wrong_output?(test_pair[1])
+                results.push(self.wrong_output_message(test_pair[1]))
+            else
+                results.push(self.correct_message)
+            end
+        end
+        
+        return results
+    end
+    
+    def self.parse_error
+        self.response["errors"]
+    end
+    
+    def self.error?
+        !self.parse_error.empty?
+    end
+    
+    def self.error_message
+        "Error running test case:\n\n#{self.parse_error}"
+    end
+    
+    def self.parse_output
+        self.response["output"]
+    end
+    
+    def self.wrong_output?(expected_output)
+        self.parse_output != expected_output
+    end
+    
+    def self.wrong_output_message(expected_output)
+        "Incorrect Answer:\n\nExpected: #{expected_output}\nActual: #{self.parse_output}"
+    end
+    
+    def self.correct_message
+        "Correct!"
     end
 end
--- spec/models/compilebox_spec.rb
+++ spec/models/compilebox_spec.rb
@@ -2,13 +2,73 @@ require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe Compilebox do
+   before :each do
+     Compilebox.base_uri = "base_uri"
+   end 
+      
    describe 'get_output' do
-      before :each do
-        Compilebox.base_uri = "base_uri"
-      end 
       it 'should call Compilebox with the correct args' do
          expect(HTTParty).to receive(:post).with("http://base_uri/compile", :body => {"language" => '0', "code" => 'print("hello")', "stdin" => 'stdin'})
          Compilebox.get_output('0', 'print("hello")', 'stdin')
       end
+      it 'should raise a network error if there is no connection' do
+         expect(HTTParty).to receive(:post).and_raise(Errno::ECONNREFUSED)
+         expect{Compilebox.get_output('','','')}.to raise_error(Compilebox::NetworkError)
+      end
+    end
+      
+   describe 'get_languages' do
+      it 'should call Compilebox' do
+         fake_results = {"languages" => ['Python', 'Java']}
+         expect(HTTParty).to receive(:get).with("http://base_uri/languages").and_return(fake_results)
+         expect(Compilebox.get_languages()).to eq(fake_results["languages"])
+      end
+      it 'should raise a network error if there is no connection' do
+         expect(HTTParty).to receive(:get).and_raise(Errno::ECONNREFUSED)
+         expect{Compilebox.get_languages}.to raise_error(Compilebox::NetworkError)
+      end
+    end
+    
+    describe 'submit_code' do
+       it 'should get the test pairs' do
+          expect(Compilebox).to receive(:get_test_pairs).with(1).and_return([])
+          Compilebox.submit_code(1, 0, '')
+       end
+       it 'should submit code for each test case (error)' do
+          test_pairs = [['3', "*\n**\n***"]]
+          code = '#test code'
+          api_result = {"output"=>"", "langid"=>"0", "code"=>code, "errors"=>"There was an error", "time"=>" .03\n"}
+          api_call_body = {:body => {"language" => '0', "code" => code, "stdin" => test_pairs[0][0]}}
+          expect(Compilebox).to receive(:get_test_pairs).with(1).and_return(test_pairs)
+          expect(HTTParty).to receive(:post).with("http://base_uri/compile", api_call_body).and_return(api_result)
+          
+          result = Compilebox.submit_code(1, "0", code)
+          expect(result.length).to eq(1)
+          expect(result[0]).to eq("Error running test case:\n\nThere was an error")
+       end
+       it 'should submit code for each test case (wrong output)' do
+          test_pairs = [['3', "*\n**\n***"]]
+          code = '#test code'
+          api_result = {"output"=>"***\n**\n*", "langid"=>"0", "code"=>code, "errors"=>"", "time"=>" .03\n"}
+          api_call_body = {:body => {"language" => '0', "code" => code, "stdin" => test_pairs[0][0]}}
+          expect(Compilebox).to receive(:get_test_pairs).with(1).and_return(test_pairs)
+          expect(HTTParty).to receive(:post).with("http://base_uri/compile", api_call_body).and_return(api_result)
+          
+          result = Compilebox.submit_code(1, "0", code)
+          expect(result.length).to eq(1)
+          expect(result[0]).to eq("Incorrect Answer:\n\nExpected: *\n**\n***\nActual: ***\n**\n*")
+       end
+       it 'should submit code for each test case (correct output)' do
+          test_pairs = [['3', "*\n**\n***"]]
+          code = '#test code'
+          api_result = {"output"=>"*\n**\n***", "langid"=>"0", "code"=>code, "errors"=>"", "time"=>" .03\n"}
+          api_call_body = {:body => {"language" => '0', "code" => code, "stdin" => test_pairs[0][0]}}
+          expect(Compilebox).to receive(:get_test_pairs).with(1).and_return(test_pairs)
+          expect(HTTParty).to receive(:post).with("http://base_uri/compile", api_call_body).and_return(api_result)
+          
+          result = Compilebox.submit_code(1, "0", code)
+          expect(result.length).to eq(1)
+          expect(result[0]).to eq("Correct!")
+       end
     end
 end
