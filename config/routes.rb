e7ad83d776168a63a62c2082a62cd31fc077702d
pauldjohnson <paul-d-johnson@uiowa.edu>
2015-10-29 02:55:08 +0000

    user sign up, sign in, and sign out working [issue 3]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :announcements
+  resources :users
+  match '/login', to: 'sessions#new', via: :get
+  match '/login_create', to: 'sessions#create', via: :post  
+  match '/logout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
