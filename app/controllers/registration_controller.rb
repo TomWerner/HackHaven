66aeaeefd74900ede4ace3d460c4934090f21e98
eglanz <emily-glanz@uiowa.edu>
2015-10-31 19:46:56 +0000

    Rspec tests for controller and edit feature created

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,13 +2,15 @@ class RegistrationController < ApplicationController
   before_filter :set_current_user
     
   def registration_params
-    params.require(:registration).permit(:userid, :contestname, :email, :firstname, :lastname, :email, :year)
+    params.require(:registration).permit(:userid, :contestname, :email, :firstname, :lastname, :email, :year, :major)
   end
 
   def index
     if @current_user != nil
      @registrations = Registration.order(:created_at => :desc).where(:userid => @current_user.id)
-   end
+    else
+      @registrations = nil
+    end
   end
 
   def edit
@@ -18,14 +20,16 @@ class RegistrationController < ApplicationController
   def new
     if @current_user != nil
      @currentuserid = @current_user.id;
+    else
+     @currentuserid = nil
     end
   end
   
   def update
     @registration = Registration.find params[:id]
-    @regitstration.update_attributes!(registration_params)
+    @registration.update_attributes!(registration_params)
     flash[:notice] = "Registration for #{@registration.firstname} was successfully updated."
-    redirect_to registration_path
+    redirect_to :action => "index"
   end
 
   def create
