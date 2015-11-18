e5953e1c5347d79a3b5f5328fa9dde2dce17367d
kaitlynsimpson <kaitlyn-engelken@uiowa.edu>
2015-11-18 03:21:02 +0000

    run db:seed to get a starting admin, admin@email.edu, password is password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "admin", email: "admin@email.edu", password: "password", admin: 0)
